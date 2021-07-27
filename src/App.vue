4e20d77f38d1db2e3f8e884311853b93920eab8e
Sardorbek Imomaliev <imomaliev@hey.com>
2021-07-27 22:23:24 +0700

    npm init vite@latest . -- --template vue-ts

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,27 @@
 <template>
-  <AppHeader />
-  <AppBreadcrumbs />
-  <router-view />
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
-<script setup>
-import AppHeader from '@/components/AppHeader/index.vue';
-import AppBreadcrumbs from '@/components/AppBreadcrumbs/index.vue';
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+})
 </script>
 
-<style lang="scss">
-.breadcrumb-section {
-  margin-top: 5rem;
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,4 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
+import { createApp } from 'vue'
+import App from './App.vue'
 
-import fa from './plugins/font-awesome';
-
-const app = createApp(App).use(store);
-
-app.use(router);
-app.use(fa);
-
-app.mount('#app');
-
-// additional global styles
-import 'scss-reset/_reset.scss';
-import 'bootstrap-scss/bootstrap-grid.scss';
-import './styles/global-fonts.scss';
-import './styles/global-styles.scss';
-import store from './store';
+createApp(App).mount('#app')

6d03b00644a3969f327888fd4bf1f5f9c9a5bba5
Sardorbek Imomaliev <imomaliev@hey.com>
2021-07-27 23:31:12 +0700

    install eslint with typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,11 @@
 module.exports = {
   root: true,
-  parser: 'vue-eslint-parser',
-  parserOptions: {
-    parser: '@typescript-eslint/parser',
-    sourceType: 'module',
-  },
-  extends: ['plugin:vue/vue3-recommended', 'plugin:vue/base', 'plugin:prettier/recommended'],
-
-  plugins: ['@typescript-eslint', 'prettier'],
-  rules: {
-    'prettier/prettier': 'error',
-    // not needed for vue 3
-    'vue/no-multiple-template-root': 'off',
-    'vue/multi-word-component-names': 'off',
-    'vue/require-default-prop': 'error',
-    'no-unused-vars': 'error',
-  },
-  overrides: [
-    {
-      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
-      env: {
-        jest: true,
-      },
-    },
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
   ],
-};
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+}

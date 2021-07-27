88b0188dd77f606ccf9f6fe7605176c33ab90445
Sardorbek Imomaliev <imomaliev@hey.com>
2021-07-27 22:23:24 +0700

    npm init vite@latest . -- --template vue-ts

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
 <template>
-  <div>
-    <SkillCard />
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
-<script setup lang="ts">
-import SkillCard from '@/components/SkillCard/index.vue';
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+})
 </script>
 
-<style></style>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

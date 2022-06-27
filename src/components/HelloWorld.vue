2160d63ecb8d73df9ad762fff8b05de67dd2e2ff
Igor <diduniki@gmail.com>
2022-06-27 10:10:13 +0300

    chore: add unit test checks to .yml

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    Привет, это должно задеплоится, пж, пж, пж
+    {{ msg }}
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
+import {Options, Vue} from 'vue-class-component';
 
 @Options({
   props: {
@@ -22,14 +22,17 @@ export default class HelloWorld extends Vue {
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }

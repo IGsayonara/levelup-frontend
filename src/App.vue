579aa44c582e552fcfc49559d2bd771833662d71
Igor <diduniki@gmail.com>
2022-07-24 09:52:00 +0300

    fix: revert all deleted features

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <p>♥ Jenkins so much ♥ test artifacts copied</p>
+  <p>Это я выёбываюсь перед Ромашкой</p>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
+import { Options,Vue } from 'vue-class-component';
 import HelloWorld from './components/HelloWorld.vue';
 
 @Options({
@@ -12,6 +14,8 @@ import HelloWorld from './components/HelloWorld.vue';
     HelloWorld,
   },
 })
+
+
 export default class App extends Vue {}
 </script>
 

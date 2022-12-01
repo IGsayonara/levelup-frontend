7423ae6360068199df190a18dbbf1fea33ffc647
Igor <diduniki@gmail.com>
2022-12-01 22:35:57 +0200

    feat: add SkillCard, prev SkillCard renamed to AppCard

---
+++ src/components/AppCard/index.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="app-card">
+    <div class="app-card__image-wrapper">
+      <img :src="imageUrl" class="image" />
+    </div>
+    <div class="app-card__text-wrapper">
+      <h3 class="title">{{ title }}</h3>
+      <div class="description">{{ description }}</div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  title: string;
+  imageUrl?: string;
+  description?: string;
+}
+
+withDefaults(defineProps<Props>(), {
+  imageUrl: '/img/404.jpg',
+  description: 'Simple description for testing Simple description for testing Simple description for testing Simple description for testing Simple description for testing',
+});
+</script>
+
+<style lang="scss" scoped>
+.app-card {
+  display: flex;
+  padding: 24px;
+  border: 1px solid #e5e5e5;
+  border-radius: 41px;
+  background-color: white;
+  cursor: pointer;
+  transition: all 0.3s ease-in;
+
+  &__image-wrapper {
+    width: 120px;
+    height: 120px;
+    margin-right: 24px;
+
+    .image {
+      width: 100%;
+      height: 100%;
+      min-width: 120px;
+      border: 1px solid #e5e5e5;
+      border-radius: 25%;
+      transition: all 0.3s ease-in;
+    }
+  }
+
+  &__text-wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+
+    .title {
+      width: 100%;
+      font-size: 24px;
+      font-weight: bold;
+      margin-bottom: 24px;
+      transition: all 0.3s ease-in;
+    }
+
+    .description {
+      font-size: 14px;
+      color: #808080;
+      margin-bottom: auto;
+    }
+  }
+  &:hover {
+    border-color: #ff8c00;
+    filter: drop-shadow(1px 1px 6px rgba(255, 140, 0, 1));
+  }
+  &:hover & {
+    &__text-wrapper {
+      .title {
+        color: #ff8c00;
+      }
+    }
+    &__image-wrapper {
+      .image {
+        border-color: #ff8c00;
+        filter: drop-shadow(1px 1px 6px rgba(255, 140, 0, 1));
+      }
+    }
+  }
+}
+</style>
--- src/components/SkillCard/index.vue
+++ src/components/SkillCard/index.vue
@@ -1,12 +1,7 @@
 <template>
-  <div class="app-card">
-    <div class="app-card__image-wrapper">
-      <img :src="imageUrl" class="image" />
-    </div>
-    <div class="app-card__text-wrapper">
-      <h3 class="title">{{ title }}</h3>
-      <div class="description">{{ description }}</div>
-    </div>
+  <div class="skill-card">
+    <h4 class="skill-card__title">{{ title }}</h4>
+    <img class="skill-card__image" :src="imageUrl" />
   </div>
 </template>
 
@@ -14,73 +9,45 @@
 interface Props {
   title: string;
   imageUrl?: string;
-  description?: string;
 }
 
 withDefaults(defineProps<Props>(), {
   imageUrl: '/img/404.jpg',
-  description: 'Simple description for testing Simple description for testing Simple description for testing Simple description for testing Simple description for testing',
 });
 </script>
 
-<style lang="scss" scoped>
-.app-card {
+<style scoped lang="scss">
+.skill-card {
   display: flex;
-  padding: 24px;
+  justify-content: space-between;
+  align-items: center;
+  height: 54px;
+  padding: 16px;
   border: 1px solid #e5e5e5;
-  border-radius: 41px;
-  cursor: pointer;
+  border-radius: 18px;
+  background-color: white;
   transition: all 0.3s ease-in;
+  cursor: pointer;
 
-  &__image-wrapper {
-    width: 120px;
-    height: 120px;
-    margin-right: 24px;
-
-    .image {
-      width: 100%;
-      height: 100%;
-      min-width: 120px;
-      border: 1px solid #e5e5e5;
-      border-radius: 25%;
-      transition: all 0.3s ease-in;
-    }
+  &__title {
+    width: 100%;
+    font-size: 14px;
+    font-weight: 500;
+    transition: all 0.3s ease-in;
   }
 
-  &__text-wrapper {
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-
-    .title {
-      width: 100%;
-      font-size: 24px;
-      font-weight: bold;
-      margin-bottom: 24px;
-      transition: all 0.3s ease-in;
-    }
-
-    .description {
-      font-size: 14px;
-      color: #808080;
-      margin-bottom: auto;
-    }
+  &__image {
+    width: 36px;
+    height: 36px;
   }
+
   &:hover {
     border-color: #ff8c00;
-    box-shadow: 1px 1px 1px 1px rgba(255, 140, 0, 0.1);
+    filter: drop-shadow(1px 1px 6px rgba(255, 140, 0, 1));
   }
   &:hover & {
-    &__text-wrapper {
-      .title {
-        color: #ff8c00;
-      }
-    }
-    &__image-wrapper {
-      .image {
-        border-color: #ff8c00;
-        box-shadow: 1px 1px 1px 1px rgba(255, 140, 0, 0.1);
-      }
+    &__title {
+      color: #ff8c00;
     }
   }
 }

ce0c69a690b527c9885c09dd4763728d92fd17a7
Igor <diduniki@gmail.com>
2022-12-04 19:06:42 +0200

    feat: add global scss imports

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `
+          @import '~bootstrap-scss/_functions.scss';
+          @import '~bootstrap-scss/_mixins.scss';
+          @import '~bootstrap-scss/_variables.scss';
+          @import '@/styles/global-colors.scss';
+        `,
+      },
+    },
+  },
 });

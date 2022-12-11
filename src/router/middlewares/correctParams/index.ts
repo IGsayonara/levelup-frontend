a9acdd98b0172ae024b1929e0bbab2243a820d0f
IGsayonara <50332314+IGsayonara@users.noreply.github.com>
2022-12-11 13:58:25 +0200

    feat: add router middlewares structure (#58)
    * feat: add router middlewares structure
    
    * fix: add login redirect home

--- src/router/middlewares/correctParams/index.ts
+++ src/router/middlewares/correctParams/index.ts
@@ -1,10 +1,10 @@
 import { isIdValid } from '@/utils/id-util';
 import { NavigationGuard } from 'vue-router';
 
-export const projectIdGuard: NavigationGuard = (to, _from, next) => {
+export const projectIdGuard: NavigationGuard = (to) => {
   if (isIdValid(to.params.id)) {
-    next();
+    return true;
   } else {
-    next('/404');
+    return { path: '/404', replace: true };
   }
 };
--- src/router/routes/routes.ts
+++ src/router/routes/routes.ts
@@ -1,5 +1,6 @@
 import { HomeRoute } from '@/router/routes/home';
 import { ProjectRoute } from '@/router/routes/project';
 import { NotFoundRoute } from '@/router/routes/404';
+import { LoginRoute } from '@/router/routes/login';
 
-export const routes = [HomeRoute, ProjectRoute, NotFoundRoute];
+export const routes = [HomeRoute, ProjectRoute, LoginRoute, NotFoundRoute];

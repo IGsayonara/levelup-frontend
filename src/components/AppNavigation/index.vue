536e265f971f447d6dfe5bf5df9b1849baa9af6d
artur.y <artur.y@filancy.com>
2022-12-16 10:16:02 +0200

    fix: format all files with eslint

---
+++ src/components/AppNavigation/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav class="navigation">
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+};
+</script>
+
+<style scoped lang="scss">
+.navigation {
+  margin-left: auto;
+  a {
+    position: relative;
+    color: black;
+    font-weight: 500;
+    text-transform: uppercase;
+    text-decoration: none;
+    margin-left: 4.5rem;
+
+    &:hover {
+      border-bottom: none;
+    }
+
+    &::after {
+      content: '';
+      position: absolute;
+      top: calc(100% + 2px);
+      width: 0;
+      left: 0;
+      height: 2px;
+      background-color: $orange;
+      transition: all 0.3s ease-in;
+    }
+
+    &:hover::after {
+      width: 100%;
+    }
+  }
+}
+</style>

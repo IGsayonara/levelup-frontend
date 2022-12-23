42ab4d5764915afd3e37a2fad542e6cc94b61ecb
IGsayonara <50332314+IGsayonara@users.noreply.github.com>
2022-12-23 16:20:52 +0200

    learn: render functions (#64)
    Co-authored-by: IDIDUNI <ihor.didunik@ext.mercedes-benz.io>

---
+++ learning/src/components/LearnVModel/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <label :for="$props.id">{{ label }}</label>
+    <input :id="$props.id" type="text" :value="reactiveModelValue" v-bind="$attrs" @input="$emit('update:modelValue', $event.target.value)" />
+  </div>
+</template>
+<script lang="ts"></script>
+<script setup lang="ts">
+import { toRef } from 'vue';
+
+interface Props {
+  id: string;
+  label: string;
+  modelValue: string;
+}
+const props = defineProps<Props>();
+
+defineEmits(['update:modelValue']);
+const reactiveModelValue = toRef(props, 'modelValue');
+</script>
+
+<style scoped></style>

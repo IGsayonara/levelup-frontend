4422c3fccd2563591002588b52ad9d9c642c33c9
IDIDUNI <ihor.didunik@ext.mercedes-benz.io>
2022-12-23 17:30:42 +0200

    chore: format files

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,15 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     sourceType: 'module',
   },
-  extends: ['plugin:vue/vue3-recommended', 'plugin:vue/base', 'plugin:prettier/recommended'],
-  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'plugin:vue/base',
+    'plugin:prettier/recommended',
+  ],
+  plugins: [
+    '@typescript-eslint',
+    'prettier',
+  ],
   rules: {
     'prettier/prettier': 'error',
     // not needed for vue 3
@@ -24,7 +31,10 @@ module.exports = {
   },
   overrides: [
     {
-      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
       env: {
         jest: true,
       },
--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,8 @@ module.exports = {
   transform: {
     '^.+\\.vue$': 'vue-jest',
   },
-  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  testMatch: [
+    '**/__tests__/**/*.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[jt]s?(x)',
+  ],
 };

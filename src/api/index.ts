867acf9316086eaddc3590737b4675e4e958d015
IDIDUNI <ihor.didunik@ext.mercedes-benz.io>
2023-01-18 21:30:03 +0200

    chore: change api url

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,11 @@ const BASE_URL = process.env.VUE_APP_API_URL;
 console.log(BASE_URL);
 
 export const test = async () => {
-  const r = await fetch(BASE_URL).then((data) => {
+  const r = await fetch(BASE_URL, {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  }).then((data) => {
     return data.text();
   });
   console.log(r);
